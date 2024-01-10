dedc12981e21cffdee7057a40eac9fd002ba7950
layokaminski <layokaminski@gmail.com>
2024-01-10 14:37:50 -0300

    build: configuração inicial do projeto

---
+++ src/test/java/com/banco/santander/SantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.santander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
